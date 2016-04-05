83ac0dfeeb64a1a8ed65d24935408fa9ff34e859
kcrong <kcrong@kim82536.pe.kr>
2016-04-05 15:02:35 +0900

    뉴런을 클래스 내에서 한번에 선언 가능하도록 함 이제 뉴런 연결 함수에서 가중치를 인자로 받지 않고, 내부에서 랜덤값으로 초기화됨

--- main.py
+++ main.py
@@ -14,13 +14,19 @@ class Neuron:
     def __repr__(self):
         return "<Neuron %s>" % self.name
 
-    def add_next_neuron(self, neuron, weight=random.uniform(0, 1)):
+    def add_next_neuron(self, neuron):
+        weight = random.random()
 
         # Add next neuron at self
-        self.next_neuron.append(dict({neuron, weight}))
+        self.next_neuron.append([neuron, weight])
 
         # Add self to before neuron at next neuron
-        neuron.before_neuron.append(dict({self, weight}))
+        neuron.before_neuron.append([self, weight])
+
+    @classmethod
+    def make_neuron(cls, make_list):
+        for name in make_list:
+            cls(name)
 
 
 # 입력 뉴런
@@ -30,6 +36,12 @@ class InputLayer(Neuron):
     def __repr__(self):
         return "<Input Neuron %s>" % self.name
 
+    @classmethod
+    def connect_conjunction(cls):
+        for input_neuron in cls.all_neuron:
+            for conjunction_neuron in ConjunctionLayer.all_neuron:
+                input_neuron.add_next_neuron(conjunction_neuron)
+
 
 # 결합 뉴런
 class ConjunctionLayer(Neuron):
@@ -38,29 +50,26 @@ class ConjunctionLayer(Neuron):
     def __repr__(self):
         return "<Combine Neuron %s>" % self.name
 
+    @classmethod
+    def connect_output(cls):
+        for conjunction_neuron, output_neuron in zip(cls.all_neuron, OutputLayer.all_neuron):
+            conjunction_neuron.add_next_neuron(output_neuron)
+
 
 # 출력 뉴런
 class OutputLayer(Neuron):
+    all_neuron = list()
+
     def __repr__(self):
         return "<Output Neuron %s>" % self.name
 
 
 if __name__ == '__main__':
+    InputLayer.make_neuron(['날개', '꼬리', '부리', '깃털', '엔진'])
+    ConjunctionLayer.make_neuron(['합규칙1', '합규칙2', '합규칙3'])
+    OutputLayer.make_neuron(['비행기', '새', '글라이더'])
 
-    # 뉴런 선언부
-    for input_name in ['날개', '꼬리', '부리', '깃털', '엔진']:
-        InputLayer(input_name)
-
-    for conjunction_name in ['새', '비행기', '글라이더']:
-        ConjunctionLayer(conjunction_name)
-
-    # 각 입력 뉴런 마다
-    for neuron in InputLayer.all_neuron:
-
-        # 각 결합 뉴런 마다
-        for conjunction_neuron in ConjunctionLayer.all_neuron:
-            # 서로 연결
-            neuron.add_next_neuron(conjunction_neuron)
-
+    InputLayer.connect_conjunction()
+    ConjunctionLayer.connect_output()
 
     print()
