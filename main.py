d57057a7b3ed321ac5952c9b1fd53bc596b0b8fe
kcrong <kcrong@kim82536.pe.kr>
2016-04-06 21:32:53 +0900

    가중치 조절 함수 추가 (fix_weight) Neuron.next_step is NO MORE classmethod train 함수는 이제 학습 입력값과 학습 결과값을 따로 받음 잘못된 주석 내용 수정

--- main.py
+++ main.py
@@ -26,11 +26,9 @@ class Neuron:
         neuron.before_neuron.append([self, weight])
 
     # 다음 단계 뉴런으로 넘어감
-    @classmethod
-    def next_step(cls):
-        for neuron in cls.all_neuron:
-            for next_neuron, weight in neuron.next_neuron:
-                next_neuron.data += neuron.data * weight
+    def next_step(self):
+        for next_neuron, weight in self.next_neuron:
+            next_neuron.data += self.data * weight
 
 
 # 입력 뉴런
@@ -125,14 +123,14 @@ class NeuronModel:
         # 모든 결합 뉴런 중, data 가 가장 큰 뉴런 객체를 반환합니다
         return max(OutputLayer.all_neuron, key=lambda output_neuron: output_neuron.data)
 
-    def train(self, train_data):
+    def train(self, train_data, result):
         """
         Train weight of Neural Connect
         :param train_data: [**input, result]
         """
 
         """
-        Example: train([1,0,1,1,-1,3]
+        Example: train([1,0,1,1,-1,1])
         일 때,
             input1 = 1 ( 날개 있어요 )
             input2 = 0 ( 꼬리 모르겠어요 )
@@ -148,9 +146,45 @@ class NeuronModel:
             리스트 index 값과 같다.
         """
 
+        # train_data[-1] ---> train_result (위 주석에서 언급한 result 값)
+        train_neuron = OutputLayer.all_neuron[result]
+
+        # 실제 가중치 계산을 통해 도출된 출력 뉴런
         result_neuron = self.get_result(train_data)
 
-        return None
+        if train_neuron != result_neuron:
+            self.fix_weight(train_neuron, result_neuron)
+
+    @staticmethod
+    def fix_weight(train_result_neuron, result_neuron):
+        """
+        :param train_result_neuron: 학습 데이터의 출력 뉴런
+        :param result_neuron: 실제 계산 후 도출된 출력 뉴런
+
+        학습 데이터 출력 뉴런의 가중치는 높이고,
+        (잘못된 결과가 나왔을 때) result_neuron 에 대한 입력 뉴런 가중치는 낮춘다.
+
+        """
+
+        # 학습 데이터 출력 뉴런에 대한 가중치 증가
+
+        # 출력 뉴런에 연결되어 있는 모든 결합 뉴런에 대해
+        for conjunction_neuron, conjunction_neuron_weight in train_result_neuron.before_neuron:
+            # 결합 뉴런에 연결되어 있는 모든 입력 뉴런에 대해
+            input_neuron_list = conjunction_neuron.before_neuron
+            for index in range(len(input_neuron_list)):
+                # 가중치 20% 증가
+                input_neuron_list[index][1] = (input_neuron_list[index][1] * 1.2)
+
+        # 잘못된 출력 뉴런 결과값에 대한 입력 뉴런 가중치 감소
+
+        # 출력 뉴런에 연결되어 있는 모든 결합 뉴런에 대해
+        for conjunction_neuron, conjunction_neuron_weight in result_neuron.before_neuron:
+            # 결합 뉴런에 연결되어 있는 모든 입력 뉴런에 대해
+            input_neuron_list = conjunction_neuron.before_neuron
+            for index in range(len(input_neuron_list)):
+                # 가중치 20% 감소
+                input_neuron_list[index][1] = (input_neuron_list[index][1] * 0.8)
 
 
 if __name__ == '__main__':
@@ -161,7 +195,7 @@ if __name__ == '__main__':
 
     model = NeuronModel(input_list, conjunction_list, output_list)
 
-    model.train([1, 0, 1, 0, 1, 2])
+    model.train([1, 0, 1, 1, -1], 1)
 
     answer_list = {'yes': 1, 'no': -1, 'dunno': 0}
 
