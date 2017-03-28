69818504b6582175131658e4d140053e3d4254fc
kcrong <kcrong@kim82536.pe.kr>
2017-03-28 23:13:55 +0900

    Add comment

--- main.py
+++ main.py
@@ -35,3 +35,17 @@ pred = model.predict(X_test)
 
 
 print(pred)
+
+"""
+>>> model.predict(X_test)
+array([[ 0.21623006,  0.52023804,  0.26353189],
+       [ 0.15998413,  0.58138859,  0.25862733],
+       [ 0.09894076,  0.34851801,  0.55254126],
+       [ 0.31136763,  0.3027361 ,  0.38589624]], dtype=float32)
+
+
+But I want
+>>> model.predict(X_test)
+array([1, 1, 2, 2])
+
+"""
